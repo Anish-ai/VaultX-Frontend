007226cb6dd337f3ce5eba9db47b6b07480100f7
Anish-ai <aniskum59431@gmail.com>
2025-03-12 23:50:52 +0530

    updated base url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:5000/api';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://vaultx-backend-763387089865.us-central1.run.app/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
